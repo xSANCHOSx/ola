8d1243b479836b676383f2304f36fbfdf76da485
S@NCHOS <xxxsanchosxxx@gmail.com>
2026-05-08 06:03:01 +0300

    timer

--- data/products.php
+++ data/products.php
@@ -65,7 +65,7 @@ $products = get_products();
  */
 function getDiscountTimer($uniqueId)
 {
-    $targetDate = strtotime('2026-06-01 00:00:00');
+    $targetDate = strtotime('2025-05-01 00:00:00');
     $currentTime = time();
     $timeLeft = $targetDate - $currentTime;
 
@@ -78,11 +78,31 @@ function getDiscountTimer($uniqueId)
     $dayWord = ($days == 1) ? 'День' : (($days >= 2 && $days <= 4) ? 'Дня' : 'Дней');
 
     return "
-			<div class='expire_date' id='timer-$uniqueId' data-end='$targetDate'>
-					До конца акции:
-					<div class='flip-clock'>
-							<div class='flip-unit'><span class='days'>$days</span><div class='flip-label'>$dayWord</div></div>
-					</div>
-			</div>
-	";
+        <div class='expire_date' id='timer-$uniqueId'>
+            До конца акции:
+            <div class='flip-clock'>
+                <div class='flip-unit'><span class='days'>$days</span><div class='flip-label'>$dayWord</div></div>
+            </div>
+        </div>
+        <script>
+            let end$uniqueId = $targetDate;
+            function updateTimer$uniqueId() {
+                let now = new Date().getTime() / 1000;
+                let timeLeft = end$uniqueId - now;
+
+                if (timeLeft <= 0) {
+                    end$uniqueId = now + (15 * 24 * 60 * 60);
+                    timeLeft = end$uniqueId - now;
+                }
+
+                let days = Math.floor(timeLeft / (24 * 60 * 60));
+                let dayWord = (days == 1) ? 'День' : ((days >= 2 && days <= 4) ? 'Дня' : 'Дней');
+
+                document.querySelector('#timer-$uniqueId .days').innerText = days;
+                document.querySelector('#timer-$uniqueId .flip-label').innerText = dayWord;
+            }
+
+            setInterval(updateTimer$uniqueId, 3600000); // Обновление раз в час
+        </script>
+    ";
 }

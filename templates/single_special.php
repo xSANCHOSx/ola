0bcb7bcb429beaf52e100f6f5f3afbd25c75cb64
S@NCHOS <xxxsanchosxxx@gmail.com>
2026-05-08 06:05:41 +0300

    fx timer 2

--- templates/single_special.php
+++ templates/single_special.php
@@ -1,8 +1,8 @@
 <?php
-
+?>
 <div class="special">
-    <div class="discont variant1">
-        <span>Скидка <?php echo ($currentProduct['old_price'] - $currentProduct['price']); ?> ₽</span>
-    </div>
-    <?php echo getDiscountTimer($currentProduct['id']); ?>
+	<div class="discont variant1">
+		<span>Скидка <?php echo ($currentProduct['old_price'] - $currentProduct['price']); ?> ₽</span>
+	</div>
+	<?php echo getDiscountTimer($currentProduct['id']); ?>
 </div>

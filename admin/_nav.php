25d50687b2dd362c42030770bb11a5510a8dc642
S@NCHOS <xxxsanchosxxx@gmail.com>
2026-05-09 21:06:55 +0300

    fix coupone

--- admin/_nav.php
+++ admin/_nav.php
@@ -3,7 +3,8 @@
     <a href="/admin/" style="margin-right:12px;">Заказы</a>
     <a href="/admin/products.php" style="margin-right:12px;">Товары</a>
     <a href="/admin/customers.php" style="margin-right:12px;">Клиенты</a>
-    <a href="/admin/blog.php" style="margin-right:12px;">Блог</a>
+    <a href="/admin/coupons.php" style="margin-right:12px; font-weight:bold; color:#008000;">💰 Купоны</a>
+    <a href="/admin/coupon_stats.php" style="margin-right:12px; font-weight:bold; color:#0066cc;">📊 Статистика</a>
     <span style="float:right;">
         <?= admin_h((string)($u['username'] ?? '')) ?> |
         <a href="/admin/logout.php">Выход</a>

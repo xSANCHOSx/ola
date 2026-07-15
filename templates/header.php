890b7be1e18754df3bf5123861434f20f0c511e9
S@NCHOS <xxxsanchosxxx@gmail.com>
2026-07-15 19:31:07 +0300

    11

--- templates/header.php
+++ templates/header.php
@@ -1,41 +1,9 @@
 <header id="header" class="sticky">
-	<div class="wrapper">
-		<div class="header-flex">
-			<div class="logo">
-				<a href="/">
-					<img id="logo" src="/images/logo.png" alt="Olaplex Logo">
-				</a>
-			</div>
-			
-			<nav class="menu-container">
-				<ul class="menu-flex">
-					<li><a href="/#max-aboutus-section">Что это?</a></li>
-					<li><a href="/#max-featured-section">Продукция</a></li>
-					<li><a href="/#max-work-section">Как использовать</a></li>
-					<li><a href="/#max-purchase-section">Доставка</a></li>
-					<li><a href="/info">Справка</a></li>
-					<li><a href="/blog">Блог</a></li>
-				</ul>
-			</nav>
-
-			<div class="header-contacts hidden-xs hidden-sm">
-				<div class="contact-item address">Санкт-Петербург, БЦ Бенуа., офис 1015</div>
-				<div class="contact-item phone">
-					<a href="tel:+78129259294">+7 (812) 925 92 94</a>
-				</div>
-				<div class="contact-item email">
-					<a href="mailto:info@priemka.spb.ru">info@priemka.spb.ru</a>
-				</div>
-				<div class="contact-item schedule">Работаем с 10:00 до 22:00 с Пн по Пт</div>
-			</div>
-
-			<div class="header-right">
-				<div class="cart cart_full" onclick="cart.showWinow('bcontainer', 1);">
-					<img class="basket_logo" src="/images/basket.png" alt="Корзина" />
-					<span id="basketwidjet" class="cart-widget-count">0</span>
-				</div>
-				
-				<button type="button" class="navbar-toggle collapsed visible-xs visible-sm" data-bs-toggle="collapse"
+	<div class="container-fluid">
+		<nav class="navbar navbar-default ">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-bs-toggle="collapse"
 					data-bs-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1"
 					aria-expanded="false">
 					<span class="sr-only">Toggle navigation</span>
@@ -43,31 +11,58 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
+				<a class="navbar-brand" href="/">
+					<img id="logo" src="/images/logo.png" alt="Olaplex Logo">
+				</a>
+				<div class="cart cart_mobile hidden-md hidden-lg" onclick="cart.showWinow('bcontainer', 1);">
+					<img class="basket_logo" src="/images/basket.png" alt="Корзина" />
+					<span id="basketwidjet2" class="cart-widget-count">0</span>
+				</div>
 			</div>
-		</div>
-
-		<!-- Mobile Menu Collapse (Original Bootstrap Logic) -->
-		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-			<ul class="nav navbar-nav main">
-				<li><a href="/#max-aboutus-section">Что это?</a></li>
-				<li><a href="/#max-featured-section">Продукция</a></li>
-				<li><a href="/#max-work-section">Как использовать</a></li>
-				<li><a href="/#max-purchase-section">Доставка</a></li>
-				<li><a href="/info">Справка</a></li>
-				<li><a href="/blog">Блог</a></li>
-			</ul>
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav main">
+					<li><a href="/#max-aboutus-section">Что это?</a></li>
+					<li><a href="/#max-featured-section">Продукция</a></li>
+					<li><a href="/#max-work-section">Как использовать</a></li>
+					<li><a href="/#max-purchase-section">Доставка</a></li>
+					<li><a href="/info">Справка</a></li>
+					<li><a href="/blog">Блог</a></li>
+				</ul>
+				<div class="header-right-area hidden-sm hidden-xs">
+					<div class="number navbar-brand">
+						<a href="tel:+74950322929">+7 (495) 032-29-29</a>
+						<a href="https://wa.me/79096962720"><img src="/images/whatsapp.svg" class="whatsapp social"
+								alt="whatsapp"></a>
+						<a href="https://t.me/kosmoprof" target="_blank"><img src="/images/tg.svg" class="telegram social"
+								alt="telegram"></a>
+						<a href="https://max.ru/id7733272706_bot" target="_blank"><img src="/images/max.svg"
+								class="max-messenger social" alt="max"></a>
+					</div>
+					<div class="cart cart_full" onclick="cart.showWinow('bcontainer', 1);">
+						<img class="basket_logo" src="/images/basket.png" alt="Корзина" />
+						<span id="basketwidjet" class="cart-widget-count">0</span>
+					</div>
+				</div>
+			</div>
 			<div class="header-right-area-mobile hidden-md hidden-lg">
 				<div class="number">
-					<a href="tel:+78129259294">
+					<a href="tel:+74950322929">
 						<img src="/images/Phone-icon.png" class="phone-icon-mobile" alt="phone">
-						+7 (812) 925 92 94
+						+7 (495) 032-29-29
 					</a>
 				</div>
 				<div class="social-icons-mobile">
-					<a href="https://wa.me/79096962720"><img src="/images/whatsapp.svg" class="whatsapp social" alt="whatsapp"></a>
-					<a href="https://t.me/kosmoprof" target="_blank"><img src="/images/tg.svg" class="telegram social" alt="telegram"></a>
+					<a href="https://wa.me/79096962720"><img src="/images/whatsapp.svg" class="whatsapp social"
+							alt="whatsapp"></a>
+					<a href="https://t.me/kosmoprof" target="_blank"><img src="/images/tg.svg" class="telegram social"
+							alt="telegram"></a>
+					<a href="https://max.ru/id7733272706_bot" target="_blank"><img src="/images/max.svg"
+							class="max-messenger social" alt="max"></a>
 				</div>
 			</div>
-		</div>
+			<!-- /.navbar-collapse -->
+		</nav>
 	</div>
+	<!-- /.container-fluid -->
 </header>
